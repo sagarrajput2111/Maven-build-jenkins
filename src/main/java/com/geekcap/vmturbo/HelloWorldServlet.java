6437922857fa092081110c9ec3e6defa02e28829
sagarrajput2111 <54945125+sagarrajput2111@users.noreply.github.com>
2024-09-23 01:05:21 +0530

    comment added to check webhook trigger

--- src/main/java/com/geekcap/vmturbo/HelloWorldServlet.java
+++ src/main/java/com/geekcap/vmturbo/HelloWorldServlet.java
@@ -8,6 +8,7 @@ public class HelloWorldServlet extends HttpServlet
 {
 @Override
   public void service( HttpServletRequest req, HttpServletResponse res ) throws IOException {
+    //comment added to check webhook trigger
     PrintWriter out = res.getWriter();
     out.println( "Hello, World!" );
     out.close();
